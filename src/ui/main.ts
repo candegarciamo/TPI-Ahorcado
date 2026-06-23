80535d946ea36116ab6a41e84c8c6502c460516a
Tomas <tomas.aguilera.27@gmail.com>
2026-06-23 20:20:18 -0300

    GREEN: AT 1

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -1,1 +1,11 @@
+import { Ahorcado } from '../domain/Ahorcado';
 
+export function mountApp(container: HTMLElement, juego: Ahorcado) {
+  // Renderizamos lo mínimo y necesario que pide el AT 1 consultando a `juego`
+  container.innerHTML = `
+    <div>
+      <h1 data-testid="word">${juego.palabraEnmascarada()}</h1>
+      <p>Vidas: <span data-testid="lives">${juego.vidas()}</span></p>
+    </div>
+  `;
+}
