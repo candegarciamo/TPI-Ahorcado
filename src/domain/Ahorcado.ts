5f7a4955d39d74d04732c0e71248b10e2c6df73e
Tomas <tomas.aguilera.27@gmail.com>
2026-06-23 19:47:53 -0300

    RED: AT - 1 - Iniciar Partida

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,12 +1,2 @@
 export class Ahorcado {
-  constructor(private palabraSecreta: string) {}
-
-  // Métodos básicos para que no falle todo al inicio
-  adivinar(letra: string): void {
-    // A implementar
-  }
-
-  palabraEnmascarada(): string {
-    return this.palabraSecreta.split('').map(() => '_').join(' ');
-  }
 }
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,9 +1,2 @@
-import { Ahorcado } from '../domain/Ahorcado';
-import { mountApp } from './main';
 
-// Arranca la aplicación leyendo el parámetro word de la URL
-const urlParams = new URLSearchParams(window.location.search);
-const word = urlParams.get('word') || 'DEFAULT';
 
-const juego = new Ahorcado(word);
-mountApp(document.getElementById('app')!, juego);
