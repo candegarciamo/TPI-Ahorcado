be3250880d44f0266b6e63242780d60aad9ddfd7
Tomas <tomas.aguilera.27@gmail.com>
2026-06-23 19:59:33 -0300

    RED: AT 1 - UT 1

---
+++ tests/Ahorcado.test.ts
@@ -0,0 +1,12 @@
+import { Ahorcado } from '../src/domain/Ahorcado';
+
+describe('Ahorcado - Iniciar Partida', () => {
+  it('Al inicializar un juego, las vidas iniciales deben ser 6', () => {
+    const juego = new Ahorcado('GATO');
+
+    // @ts-ignore - Ignoramos el chequeo de tipos para poder correr el test y verlo fallar (ROJO)
+    expect(juego.vidas()).toBe(6);
+  });
+});
+
+
