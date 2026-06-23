5ceccbb56a60d4c43e0763718996dfd851cf5973
Candela Garcia <candegarciamo@gmail.com>
2026-06-23 20:57:15 -0300

    GREEN: AT 2 - UT 1

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -7,8 +7,18 @@ export class Ahorcado {
     return 6;
   }
 
+  adivinar(letra: string): void {
+    const letraUpper = letra.toUpperCase();
+    if (!this.letras.includes(letraUpper)) {
+      this.letras.push(letraUpper);
+    }
+  }
+
   palabraEnmascarada(): string {
-    return this.palabraSecreta.split('').map(() => '_').join(' ');
+    return this.palabraSecreta
+      .split('')
+      .map(char => this.letras.includes(char.toUpperCase()) ? char : '_')
+      .join(' ');
   }
 
   letrasAdivinadas(): string[] {
