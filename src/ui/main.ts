b7ae804089618c49266480fdb094d41269bba65a
meliaronson <melinaaronson@gmail.com>
2026-06-23 22:12:06 -0300

    GREEN: AT 3

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -6,6 +6,7 @@ export function mountApp(container: HTMLElement, juego: Ahorcado) {
       <div>
         <h1 data-testid="word">${juego.palabraEnmascarada()}</h1>
         <p>Vidas: <span data-testid="lives">${juego.vidas()}</span></p>
+        <p>Letras erradas: <span data-testid="missed-letters">${juego.letrasErradas().join(', ')}</span></p>
         <input type="text" placeholder="Ingresá una letra" autofocus />
       </div>
     `;
