0bf99a10102b587c0ce71b6687be05c81d1272fc
meliaronson <melinaaronson@gmail.com>
2026-06-23 21:50:50 -0300

    GREEN: AT 3 - UT 1

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,16 +1,20 @@
 export class Ahorcado {
   private letras: string[] = [];
+  private _vidas: number = 6;
 
   constructor(private palabraSecreta: string) { }
 
   vidas(): number {
-    return 6;
+    return this._vidas;
   }
 
   adivinar(letra: string): void {
     const letraUpper = letra.toUpperCase();
     if (!this.letras.includes(letraUpper)) {
       this.letras.push(letraUpper);
+      if (!this.palabraSecreta.toUpperCase().includes(letraUpper)) {
+        this._vidas--;
+      }
     }
   }
 
